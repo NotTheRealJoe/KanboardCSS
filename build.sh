ff177a00ad0bf9a07e1513d4e6a71010fbb60a93
Joe Tortorello <32490063+NotTheRealJoe@users.noreply.github.com>
2020-01-28 08:55:10 -0700

    Update build script to use yui-compressor

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+if ! command -v yui-compressor > /dev/null; then
+  echo 'yui-compressor not found. Make sure it is installed in a location in PATH.'
+  echo 'try apt install yui-compressor'
+  exit 1
+fi
+
 mkdir -p 'out'
-cat ./*.css | perl -pe 's/\n//mg;' -pe 's/ {2,}/ /g;' | perl -pe 's/ ?(\{|\}|,|\;|\(|\)|:|>) ?/\1/g;' > out/full.css
+
+# Using the pipe with cat is necessary because yui-compressor won't accept multiple input files directly
+cat ./*.css | yui-compressor --type=css -o out/full.css
